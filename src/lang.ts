90980a6ae32c24c9fcf78dbe4fdd19fafcfd20a9
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2025-03-05 01:36:47 -0300

    Finish translations and modularize projects component

--- src/lang.ts
+++ src/lang.ts
@@ -1,5 +1,5 @@
 type Translations = {
-  [key in 'en' | 'es']: {
+  [key in "en" | "es"]: {
     nav: {
       home: string;
       experience: string;
@@ -7,8 +7,40 @@ type Translations = {
     };
     greeting: string;
     description: string;
-    workExperience: string;
-    projects: string;
+    experienceSection: string;
+    techStack: string;
+    jobs: {
+      taktiful: {
+        description: string;
+        startDate: string;
+        endDate: string;
+      };
+      zite: {
+        description: string;
+        startDate: string;
+        endDate: string;
+      };
+      gainapp: {
+        description: string;
+        startDate: string;
+        endDate: string;
+      };
+    };
+    projectsSection: string;
+    projects: {
+      buno: {
+        description: string;
+      },
+      powerbutton: {
+        description: string;
+      },
+      energialy: {
+        description: string;
+      },
+    },
+    codeButton: string;
+    previewButton: string;
+    footer: string;
   };
 };
 
@@ -17,7 +49,7 @@ export const translations: Translations = {
     nav: {
       home: "Home",
       experience: "Experience",
-      projects: "Projects"
+      projects: "Projects",
     },
     greeting: "Hi, I am",
     description: `A full-stack web developer from Buenos Aires with 2+ years of experience
@@ -26,14 +58,65 @@ export const translations: Translations = {
     from and improve my work. On every team I work on, I make great efforts to be
     a positive influence, deliver an excellent user experience, and help the team
     build a maintainable architecture.`,
-    workExperience: "Work Experience",
-    projects: "Projects"
+    experienceSection: "Work Experience",
+    techStack: "Tech Stack",
+    jobs: {
+      taktiful: {
+        description: `Development and maintenance of three products for clients in the
+        digital printing industry, optimizing processes and enhancing user experience.
+        Implementation of new features across all layers of the stack, backend and frontend
+        performance optimization, bug resolution, and continuous improvement, ensuring
+        platform scalability.`,
+        startDate: "July 2024",
+        endDate: "Present",
+      },
+      zite: {
+        description: `Full-stack development of a platform designed to improve private market
+        investment processes. Focus on user experience, leading the product's UI redesign.
+        Responsible for bug resolution, continuous application improvements, and contributing
+        new ideas to the product. Deep understanding of the business to provide digital
+        solutions that enable investors to manage their portfolios and centralize their
+        workflow in a single platform.`,
+        startDate: "Apr. 2024",
+        endDate: "July 2024",
+      },
+      gainapp: {
+        description: `Growth + Software Engineering (+ Data). Develop and maintain Gain's main
+        website and blog. Implement and configure campaign-related web technology such as UTM
+        tags, tracking pixels, SEO, software vendor reviews and Google Analytics. Build and
+        follow-up marketing and product dashboards in event-driven analytics software such as
+        Mixpanel and Hotjar.`,
+        startDate: "Sept. 2023",
+        endDate: "Mar. 2024",
+      },
+    },
+    projectsSection: "Projects",
+    projects: {
+      buno: {
+        description: `SaaS that connects businesses with freelance attorneys for on-demand legal 
+        services. It offers a seamless way to find, hire, and collaborate with lawyers for contracts, 
+        business formation, and more—providing flexible, cost-effective legal support.`,
+      },
+      powerbutton: {
+        description: `Final Project of the Next.js course at Coderhouse. Power Button is an e-commerce 
+        built with Next.js, integrating authentication services and databases, with optimal performance 
+        on SEO optimization.`,
+      },
+      energialy: {
+        description: `Energialy is a platform to integrate into the Value Chain, manage contracts between 
+        SMEs, and access financing for energy services and projects in Vaca Muerta. A single platform to 
+        hire and be hired.`,
+      },
+    },
+    codeButton: "Code",
+    previewButton: "Preview",
+    footer: "Almost All Rights Reserved",
   },
   es: {
     nav: {
       home: "Inicio",
       experience: "Experiencia",
-      projects: "Proyectos"
+      projects: "Proyectos",
     },
     greeting: "Hola, soy",
     description: `Desarrollador web full-stack de Buenos Aires con 2+ años de experiencia
@@ -42,7 +125,59 @@ export const translations: Translations = {
     y mejorar mi trabajo. En cada equipo en el que trabajo, me esfuerzo mucho por ser 
     una influencia positiva, ofrecer una excelente experiencia de usuario y ayudar al equipo 
     a construir una arquitectura mantenible.`,
-    workExperience: "Experiencia Laboral",
-    projects: "Proyectos"
+    experienceSection: "Experiencia Laboral",
+    techStack: "Tecnologías",
+    jobs: {
+      taktiful: {
+        description: `Desarrollo y mantenimiento de tres productos para clientes en la
+        industria de la impresión digital, optimizando procesos y mejorando la experiencia
+        del usuario. Implementación de nuevas funcionalidades en todas las capas del stack,
+        optimización del rendimiento del backend y frontend, resolución de bugs y mejora
+        continua, asegurando la escalabilidad de las plataformas.`,
+        startDate: "jul. 2024",
+        endDate: "Actualidad",
+      },
+      zite: {
+        description: `Desarrollo full-stack de una plataforma diseñada para mejorar los procesos
+        de inversión del mercado privado. Priorización de la experiencia del usuario;
+        rediseño de la UI del producto. Encargado de resolver bugs, mejora continua de 
+        la aplicación y aportar nuevas ideas al producto. Profunda comprensión del negocio
+        para ofrecer soluciones digitales que permitan a los inversores administrar su
+        portafolio y centralizar su workflow.`,
+        startDate: "abr. 2024",
+        endDate: "jul. 2024",
+      },
+      gainapp: {
+        description: `Growth + Software Engineering (+ Data). Desarrollar y mantener el website
+        y el blog de Gain. Implementar y configurar tecnologías web para campañas de marketing
+        como UTM tags, tracking pixels, SEO, software vendor reviews y Google Analytics.
+        Implementar y diseñar dashboards de datos con métricas de Marketing, Producto y website
+        en herramientas event-driven como Mixpanel y Hotjar.`,
+        startDate: "sept. 2023",
+        endDate: "mar. 2024",
+      },
+    },
+    projectsSection: "Proyectos",
+    projects: {
+      buno: {
+        description: `SaaS que conecta personas y empresas con abogados independientes para servicios 
+        legales a demanda. Ofrece una manera fluida de encontrar, contratar y colaborar con 
+        abogados para contratos, formación de empresas y más, proporcionando un soporte legal 
+        flexible y rentable.`,
+      },
+      powerbutton: {
+        description: `Proyecto final del curso de Next.js en Coderhouse. Power Button es un 
+        e-commerce construido con Next.js, integrando servicios de autenticación y bases de datos, 
+        con un rendimiento óptimo en optimización SEO.`,
+      },
+      energialy: {
+        description: `Energialy es una plataforma para integrarse a la cadena de valor, gestionar 
+        contratos entre PYMEs y acceder a financiamiento para servicios y proyectos energéticos en 
+        Vaca Muerta. Una plataforma única para contratar y ser contratado.`,
+      },
+    },
+    codeButton: "Repositorio",
+    previewButton: "Deploy",
+    footer: "Casi todos los derechos reservados"
   },
 };
---
+++ src/tags.ts
@@ -0,0 +1,111 @@
+import NextJS from "./icons/NextJS.astro";
+import Tailwind from "./icons/Tailwind.astro";
+import React from "./icons/React.astro";
+import TypeScript from "./icons/TypeScript.astro";
+import JavaScript from "./icons/JavaScript.astro";
+import NodeJS from "./icons/NodeJS.astro";
+import Express from "./icons/ExpressJS.astro";
+import Firebase from "./icons/Firebase.astro";
+import MongoDB from "./icons/MongoDB.astro";
+import PostgreSQL from "./icons/PostgreSQL.astro";
+import MySQL from "./icons/MySQL.astro";
+import Docker from "./icons/Docker.astro";
+import AWS from "./icons/AWS.astro";
+import Vercel from "./icons/Vercel.astro";
+import ShadCN from "./icons/ShadCN.astro";
+import Drizzle from "./icons/Drizzle.astro";
+import Sequelize from "./icons/Sequelize.astro";
+import Neon from "./icons/Neon.astro";
+
+export const TAGS = {
+  NEXT: {
+    name: "Next.js",
+    class: "bg-black text-white",
+    icon: NextJS,
+  },
+  TAILWIND: {
+    name: "Tailwind CSS",
+    class: "bg-[#003159] text-white",
+    icon: Tailwind,
+  },
+  REACT: {
+    name: "React",
+    class: "bg-[#20232A] text-white",
+    icon: React,
+  },
+  TYPESCRIPT: {
+    name: "TypeScript",
+    class: "bg-[#3178C6] text-white",
+    icon: TypeScript,
+  },
+  JAVASCRIPT: {
+    name: "JavaScript",
+    class: "bg-[#F7DF1E] text-black",
+    icon: JavaScript,
+  },
+  NODE: {
+    name: "Node.js",
+    class: "bg-[#026E00] text-white",
+    icon: NodeJS,
+  },
+  EXPRESS: {
+    name: "Express.js",
+    class: "bg-[#000000] text-white",
+    icon: Express,
+  },
+  FIREBASE: {
+    name: "Firebase",
+    class: "bg-stone-800 text-white",
+    icon: Firebase,
+  },
+  MONGODB: {
+    name: "MongoDB",
+    class: "bg-[#113422] text-[#00ED64]",
+    icon: MongoDB,
+  },
+  POSTGRESQL: {
+    name: "PostgreSQL",
+    class: "bg-[#002B36] text-[#4c9ee0]",
+    icon: PostgreSQL,
+  },
+  MYSQL: {
+    name: "MySQL",
+    class: "bg-gray-100 text-[#f29221]",
+    icon: MySQL,
+  },
+  DOCKER: {
+    name: "Docker",
+    class: "bg-[#0c2545] text-[#0db7ed]",
+    icon: Docker,
+  },
+  AWS: {
+    name: "AWS",
+    class: "bg-[#232F3E] text-[#FF9900]",
+    icon: AWS,
+  },
+  VERCEL: {
+    name: "Vercel",
+    class: "bg-black text-white",
+    icon: Vercel,
+  },
+  SHADCNUI: {
+    name: "ShadCN UI",
+    class: "bg-black text-white",
+    icon: ShadCN,
+  },
+  DRIZZLE: {
+    name: "Drizzle ORM",
+    class: "bg-black text-white",
+    icon: Drizzle,
+  },
+  SEQUELIZE: {
+    name: "Sequelize",
+    class: "bg-stone-800 text-white",
+    icon: Sequelize,
+  },
+  NEON: {
+    name: "Neon",
+    class: "bg-black text-white",
+    icon: Neon,
+  },
+} as const;
