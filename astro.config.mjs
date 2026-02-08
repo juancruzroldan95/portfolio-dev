406d31828294de834bd3355a579bf74d5c888e7d
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2026-02-07 21:11:29 -0300

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
 
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jcroldan.dev',
   integrations: [
     tailwind(),
+    sitemap(),
     partytown({
       config: {
         forward: ["dataLayer.push", "gtag"],
