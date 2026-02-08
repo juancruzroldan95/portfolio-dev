855928efe40e096c53cdfab27afaf8be1e71e051
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2026-02-07 21:21:13 -0300

    Remove astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
 
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
@@ -10,7 +9,6 @@ export default defineConfig({
   site: 'https://jcroldan.dev',
   integrations: [
     tailwind(),
-    sitemap(),
     partytown({
       config: {
         forward: ["dataLayer.push", "gtag"],
