d1fb433ba592331f8f3d99a7e11dd72f8e4c58ac
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2026-02-23 11:42:26 -0300

    Update astro to v5 and add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import partytown from "@astrojs/partytown";
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jcroldan.dev',
+
   integrations: [
     tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push", "gtag"],
-      },
-    }),
-  ]
+  ],
+
+  adapter: vercel()
 });
